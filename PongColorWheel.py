bceaab10e558930bc0a6d34cc9fdd84b3dce61e4
Jaden Baker <jadenbaker824@gmail.com>
2019-02-06 10:59:30 -0800

    Player 1 paddle color changes in settings (needs work)

--- PongColorWheel.py
+++ PongColorWheel.py
@@ -1,7 +1,17 @@
 from kivy.uix.colorpicker import ColorWheel
 
 
-class PongColorWheel(ColorWheel)
+class PongColorWheel(ColorWheel):
     
-    def __init__(self, **kwargs)
-        super(PongColorWheel, self)    
+    def __init__(self, **kwargs):
+        super(PongColorWheel, self).__init__(**kwargs)
+
+        self.init_wheel(dt=0)
+
+        def on__hsv(self, instance, value):
+
+            
+
+            super(PongColorWheel, self).on__hsv(instance, value)
+
+            print("In PongColorWheel - on__hsv(), color selected: " + str (self.rgba))
--- SettingsScreen.py
+++ SettingsScreen.py
@@ -1,5 +1,6 @@
 from kivy.properties import ListProperty, ObjectProperty
 from kivy.uix.screenmanager import Screen
+from PongColorWheel import PongColorWheel
 
 
 
@@ -14,6 +15,7 @@ class SettingsScreen(Screen):
     player_1_name_widget = ObjectProperty(None)
 
     player_1_paddle_color = ObjectProperty(None)
+    player_1_label_color = ListProperty([0, 0, 0, 1])
 
 
     def save(self):
@@ -21,12 +23,15 @@ class SettingsScreen(Screen):
 
         self.manager.get_screen("game_screen").player_1_name = self.player_1_name_widget.text
             
+        
 
         self.manager.current = "game_screen"
 
 
     def player_1_paddle_on_color(self, rgba_color):
         print("In SettingsScreen - player_1_paddle_on_color(), RGBA =", str(rgba_color))
+
+        self.player_1_label_color = rgba_color
 
         self.manager.get_screen("game_screen").player_1_paddle_color = rgba_color
 
